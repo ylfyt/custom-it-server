fa354d37a2206552646a37139de47d24777d2efd
yudialfayat <ayudi3521@gmail.com>
2021-12-22 17:07:55 +0700

    Fix connection bug

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { graphqlHTTP } from 'express-graphql';
-import mongoose, { connection } from 'mongoose';
-// import { MikroORM } from '@mikro-orm/core';
 
-import schema from './schema/schema';
 import { createConnection } from 'typeorm';
 import { Store } from './entities/Store';
 import 'reflect-metadata';
-// import { Product } from './entities/Product';
-// import { Store } from './entities/Store';
+
+const app = express();
 
 // app.use(express.static('public'));
 
@@ -21,12 +18,6 @@ import 'reflect-metadata';
 // 	})
 // );
 
-// mongoose.connect(DB_CONNECT, () => {
-// 	console.log('Database is connected ...');
-// });
-
-const app = express();
-
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 
@@ -34,16 +25,21 @@ app.get('/', (req, res) => {
 	res.send('Hello ');
 });
 
-createConnection()
+createConnection({
+	type: 'mongodb',
+	url: process.env.DB_CONNECT,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	synchronize: true,
+	logging: true,
+	entities: ['dist/entities/*.{ts,js}'],
+})
 	.then(async (connection) => {
-		console.log('DB is connected');
+		console.log('Mongodb is connected');
 
 		app.listen(PORT, () => {
 			console.log(`Server is listening on port ${PORT} | http://localhost:${PORT}`);
 		});
-
-		const strs = await connection.manager.find(Store);
-		console.log(strs);
 	})
 	.catch((error) => {
 		console.log(error.message);
