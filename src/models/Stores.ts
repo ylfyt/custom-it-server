1f74eab6130383bf27ecfea6e3e004ad74552f52
yudialfayat <ayudi3521@gmail.com>
2021-12-23 01:51:55 +0700

    [Delete] Delete old models and schema

--- src/models/Stores.ts
+++
@@ -1,9 +0,0 @@
-// import mongoose from 'mongoose';
-
-// const storeSchema = new mongoose.Schema({
-// 	name: { type: String, required: true },
-// 	username: { type: String, required: true },
-// 	address: { type: String, required: true },
-// });
-
-// export default mongoose.model('Stores', storeSchema);
--- src/schema/schema.ts
+++
@@ -1,9 +0,0 @@
-// import { GraphQLSchema } from 'graphql';
-
-// import { RootQuery } from './RootQueryType';
-// import { Mutation } from './MutationType';
-
-// export default new GraphQLSchema({
-// 	query: RootQuery,
-// 	mutation: Mutation,
-// });
