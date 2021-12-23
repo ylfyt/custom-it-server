c92c58c929f3c70477baedac350f4e3fa286319b
yudialfayat <ayudi3521@gmail.com>
2021-12-23 19:28:45 +0700

    Adding mutation createProduct

--- src/resolvers/product.resolver.ts
+++ src/resolvers/product.resolver.ts
@@ -1,4 +1,4 @@
-import { Arg, Ctx, FieldResolver, Query, Resolver, Root } from 'type-graphql';
+import { Arg, Ctx, FieldResolver, Mutation, Query, Resolver, Root } from 'type-graphql';
 import { Product } from '../entities/Product';
 import { Store } from '../entities/Store';
 import { MyContext } from '../types';
@@ -18,5 +18,28 @@ export class ProductResolver {
 	@FieldResolver(() => Store)
 	async store(@Root() product: Product, @Ctx() { em }: MyContext) {
 		return await em.findOne(Store, { id: product.storeId });
+	}
+
+	@Mutation(() => Product)
+	async createProduct(
+		@Arg('name') name: string,
+		@Arg('description') description: string,
+		@Arg('price') price: number,
+		@Arg('stock') stock: number,
+		@Arg('storeId') storeId: string,
+		@Arg('image') image: string,
+		@Ctx() { em }: MyContext
+	) {
+		const newProduct = em.create(Product, {
+			name: name,
+			description: description,
+			price: price,
+			stock: stock,
+			storeId: storeId,
+			image: image,
+		});
+
+		await em.persistAndFlush(newProduct);
+		return newProduct;
 	}
 }
